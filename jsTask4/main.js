1112173f735903cfab22db25fe6d0c1266893bde
Dashqin <100667487+Dashqin@users.noreply.github.com>
2022-05-24 18:04:58 +0300

    Delete jsTask4 directory

--- jsTask4/main.js
+++
@@ -1,9 +0,0 @@
-// "use strict"
-let submit=document.querySelector(".button1");
-let taskInput = document.querySelector("input.input1");
-let taskWrapper = document.querySelector(".tasks .list-group");
-submit.onclick = () => {
-        let newTaskElem = "<li class='list-group-item'>" + taskInput.value + d + "</li>"
-        taskWrapper.innerHTML = newTaskElem + taskWrapper.innerHTML ;
-        taskInput.style.display="none";
-}
